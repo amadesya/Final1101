16b26a5a50823d81f79d593a3560dcc8b90981ab
E66 <E66@DESKTOP-QSSOH61>
2024-12-20 06:23:53 +0300

    Добавьте файлы проекта.

---
+++ Desktop/Pages/ShopPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Desktop.Pages;
+using StoreLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Store.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ShopPage.xaml
+    /// </summary>
+    public partial class ShopPage : Page
+    {
+        public ShopPage()
+        {
+            InitializeComponent();
+            var currentUser = Application.Current.Properties["CurrentUser"] as User;
+            UpdateLoginButton(currentUser);
+
+        }
+        private void UpdateLoginButton(User? currentUser)
+        {
+            if (currentUser == null)
+                LoginButton.Content = "Войти";
+            else
+                LoginButton.Content = "Выйти";
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentUser = Application.Current.Properties["CurrentUser"] as User;
+
+            if (currentUser == null)
+                NavigationService.Navigate(new AuthorizationPage());
+            else
+                Application.Current.Properties["CurrentUser"] = null;
+                NavigationService.Navigate(new AuthorizationPage());
+        }
+
+        private void OrderPanelButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new OrderPanelPage());
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,4 @@
+1) Запустить скрипт под названием "ParfumShopDatabase.sql".
+2) Отредактировать строку подключения к серверу, находящуюся по пути StoreLibrary - Data - AppDbContext.cs
+"Server=[ИМЯ ПК];Database=[НАЗВАНИЕ БАЗЫ ДАННЫХ];Trusted_Connection=True; Trust Server Certificate=True"
+3) Аккаунт администратора t.savina пароль XK3sOA
